098bca78b2437b4f89f07effe4d55c7fcc47ac3c
Joel Cuthriell <joelcuthriell@seqtechllc.com>
2020-04-13 14:18:13 -0500

    setting up a basic form

--- react-seq-boilerplate/src/App.js
+++ react-seq-boilerplate/src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { useForm } from 'react-hook-form';
+
 function App() {
+  const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = data => {
+    console.log(data);
+  }; // your form submit function which will invoke after successful validation
+
+  console.log(watch("example")); // you can watch individual input by pass the name of the input
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>Example</label>
+      <input name="example" defaultValue="test" ref={register} />
+      <label>ExampleRequired</label>
+      <input
+        name="exampleRequired"
+        ref={register({ required: true, maxLength: 10 })}
+      />
+      {errors.exampleRequired && <p>This field is required</p>}
+      <input type="submit" />
+    </form>
+    </Container>
   );
 }
 
